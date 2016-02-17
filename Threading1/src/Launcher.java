ddd0257b88f628279720208e39ab23bba98db0a6
František Galčík <frantisek.galcik@gbox.sk>
2016-02-17 10:04:57 +0100

    Vlakna

--- Threading1/src/Launcher.java
+++ Threading1/src/Launcher.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Vypisovac v = new Vypisovac("Ahoj");
+		Vypisovac v2 = new Vypisovac("Java");
+		v.start();
+		v2.start();
+		
+		Scanner sc = new Scanner(System.in);
+		sc.nextLine();
+		System.out.println("Koniec");
+		v.interrupt();
+		v2.interrupt();
+		
 	}
 
 }
